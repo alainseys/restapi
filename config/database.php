82d87a1f948dc8ab73c7948ba07a83dae2966f9a
alain <alain@biopack.be>
2020-05-03 14:13:14 +0200

    database class

---
+++ config/database.php
@@ -0,0 +1,22 @@
+<?php
+
+class Database{
+    private $host = 'db';
+    private $dbname = 'development';
+    private $dbuser = 'devuser';
+    private $dbpass = 'devpassword';
+    private $conn;
+
+    public function connect(){
+        $this->conn = null;
+
+        try{
+            $this->conn = new PDO('mysql:host='.$this->host .';dbname='.$this->dbname,$this->dbuser,$this->dbpass);
+            $this->conn->setAttribute(PDO::ATTR_ERRORMODE,PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $e){
+            echo 'Connection error:'. $e->getMessage();
+        }
+        return $this->conn;
+    }
+}
